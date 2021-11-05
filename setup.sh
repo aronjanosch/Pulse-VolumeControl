3d05ff3486e6446e0707775f6585a4f225c39387
Aron Wiederkehr <aron.wiederkehr@gmail.com>
2021-11-05 03:43:59 +0100

    refactoring

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cp ./Wuba_volControl.py /usr/local/bin/Wuba_volControl.py
+sudo cp ./VolControl.py /usr/local/bin/VolControl.py
 sudo cp ./VolControl.service /etc/systemd/system/VolControl.service
